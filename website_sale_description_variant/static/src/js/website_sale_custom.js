d22ae2d5c6c69425174761b0cb146a24ecddcf17
Andreas Perhab <andreas.perhab@wt-io-it.at>
2019-08-09 08:32:21 +0200

    [FIX] website_sale_description_variant: cleanup requires in javascript

--- website_sale_description_variant/static/src/js/website_sale_custom.js
+++ website_sale_description_variant/static/src/js/website_sale_custom.js
@@ -2,23 +2,17 @@ odoo.define('website_sale_description_variant.website_sale', function (require) 
     "use strict";
 
     require('web.dom_ready');
-    var base = require('web_editor.base');
-    var ajax = require('web.ajax');
-    var utils = require('web.utils');
-    var time = require('web.time');
-    var core = require('web.core');
-    var config = require('web.config');
-    var _t = core._t;
-    
-    if(!$('.oe_website_sale').length) {
+
+    var $elements = $('.oe_website_sale');
+    if(!$elements.length) {
         return $.Deferred().reject("DOM doesn't contain '.oe_website_sale'");
     }
 
-    $('.oe_website_sale').each(function () {
-        var oe_website_sale = this;
+    $elements.each(function () {
+        var $oe_website_sale = $(this);
 
-        $(oe_website_sale).on("change", "input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]", function (event) {
-	        var $input = $(this);
+        $oe_website_sale.on("change", "input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]", function (event) {
+
 	        var $ul = $(event.target).closest('.js_add_cart_variants');
 	        var $parent = $ul.closest('.js_product');
 	        var product_id = parseInt($parent.find('input.product_id').val());
@@ -26,14 +20,14 @@ odoo.define('website_sale_description_variant.website_sale', function (require) 
 	        if(_.isString(variant_ids)) {
 	            variant_ids = JSON.parse(variant_ids.replace(/'/g, '"'));
 	        }
-	        $(oe_website_sale).find('input.js_variant_change').parent('label').parent('li').show();
+	        $oe_website_sale.find('input.js_variant_change').parent('label').parent('li').show();
 	        for (var k in variant_ids) {
 	            if (variant_ids[k][0] != product_id) {
 	                continue
 	            }
 	            var info = variant_ids[k][4];
 	            var description = info.description_sale_variant || '';
-	            $(oe_website_sale).find('#description_sale_variant').html(description);
+	            $oe_website_sale.find('#description_sale_variant').html(description);
 	        }
 	    });
     });

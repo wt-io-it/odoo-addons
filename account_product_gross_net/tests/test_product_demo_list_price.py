6673be8a8a31f4073503b6148aefa156cbf07392
Andreas Perhab <andreas.perhab@wt-io-it.at>
2019-09-02 15:03:21 +0200

    [MIG] account_product_gross_net: tests: use Customizable Desk
    instead of old Apple In-Ear Headphones

--- account_product_gross_net/tests/test_product_demo_list_price.py
+++ account_product_gross_net/tests/test_product_demo_list_price.py
@@ -5,5 +5,6 @@ from odoo.tests.common import TransactionCase
 class TestProductDemoListPrice(TransactionCase):
 
     def test_ensure_we_have_a_price_set_for_demo_products(self):
-        apple_in_ear = self.env['product.template'].search([('name', '=', 'Apple In-Ear Headphones')])
-        self.assertNotEqual(0, apple_in_ear.list_price, 'Expected apple in ear headphones to have a non zero list price')
+        customizable_desk = self.env['product.template'].search([('name', '=', 'Customizable Desk')])
+        self.assertTrue(customizable_desk, 'Expected Customizable Desk to exist as a product for tests')
+        self.assertNotEqual(0, customizable_desk.list_price, 'Expected Customizable Desk to have a non zero list price')

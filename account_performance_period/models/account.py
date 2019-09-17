962a39d29c57d03dc757436eb3bed2eb4f4e5bbd
Andreas Perhab <andreas.perhab@wt-io-it.at>
2019-09-17 11:14:52 +0200

    [FIX] account_performance_period: don't require dates for account moves

--- account_performance_period/models/account.py
+++ account_performance_period/models/account.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-from odoo.addons import decimal_precision as dp
+from odoo import models, fields, api
 
 import logging
 
@@ -56,13 +54,13 @@ class AccountInvoice(models.Model):
                 'date_end': inv_line.date_end,
             })
             analytic_lines = []
+            # TODO it seems analytic_line_ids is never present in line (not populated in super method)
             for analytic_line in line.get('analytic_line_ids') or []:
                 analytic_line[2].update({
                     'date_range_id': inv_line.date_range_id.id,
                     'date_start': inv_line.date_start,
                     'date_end': inv_line.date_end,
                 })
-                analytic_lines.append(analytic_line)
             inv_lines.append(line)
 
         return inv_lines
@@ -140,11 +138,11 @@ class AccountMoveLine(models.Model):
     )
     date_start = fields.Date(
         string='Performance Start',
-        required=True,
+        required=False,
     )
     date_end = fields.Date(
         string='Performance End',
-        required=True,
+        required=False,
     )
 
     @api.multi

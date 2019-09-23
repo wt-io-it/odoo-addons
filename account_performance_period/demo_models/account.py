9fc1f60796252f6ce0a1c626e87118ea572fb0db
Andreas Perhab <andreas.perhab@wt-io-it.at>
2019-09-23 09:36:26 +0200

    [FIX] account_performance_period: skip demo models in tests
    only use them in demo data

--- account_performance_period/demo_models/account.py
+++ account_performance_period/demo_models/account.py
@@ -2,6 +2,12 @@
 from odoo import models, api
 from datetime import date
 import threading
+import traceback
+
+
+def is_running_in_test():
+    # we check if we are running in any test
+    return any([frame for frame in traceback.extract_stack() if '/tests/test' in frame.filename])
 
 
 class AccountInvoiceLine(models.Model):
@@ -9,7 +15,10 @@ class AccountInvoiceLine(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        if hasattr(threading.currentThread(), 'testing') and threading.currentThread().testing:
+        if (
+            hasattr(threading.currentThread(), 'testing') and threading.currentThread().testing
+            and not is_running_in_test()
+        ):
             # when we are in testing mode (inserting demo data into the db) we add dates for odoo core demo data
             for vals in vals_list:
                 if not vals.get('date_start', None):

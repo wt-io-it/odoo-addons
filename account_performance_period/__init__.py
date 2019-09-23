1eee1a675e3fb069fd7d472560be351330e156b3
Andreas Perhab <andreas.perhab@wt-io-it.at>
2019-09-23 12:24:39 +0200

    [FIX] account_performance_period: do not require a dates in db

--- account_performance_period/__init__.py
+++ account_performance_period/__init__.py
@@ -1,12 +1,1 @@
 from . import models
-
-
-def is_demomode():
-    from odoo.tools import config
-    return not config.get('without_demo')
-
-
-if is_demomode():
-    from . import demo_models
-    import logging
-    logging.getLogger(__name__).debug("Enabled demo imports")
